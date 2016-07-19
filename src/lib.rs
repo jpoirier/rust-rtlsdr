7a3f31f31af08645cc8d1dfbbf1d0779330a115b
Joseph Poirier <jdpoirier@gmail.com>
2016-07-19 14:54:24 -0500

    dual license

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 // Copyright (c) 2016 Joseph D Poirier <jdpoirier@gmail.com>
-// Licensed under the MIT License <LICENSE.md>
+// Licensed under the MIT License <LICENSE-MIT.md>
+
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 #![allow(dead_code)]
 use std::sync::Arc;
